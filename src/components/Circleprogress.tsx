10a382d0a94bccd5955a36efb9c8bc3367dcab86
prasath70 <prasathsaravanamuthu@gmail.com>
2024-07-07 15:50:28 +0530

    accordiaon basic mobile view

--- src/components/Circleprogress.tsx
+++ src/components/Circleprogress.tsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { TiTick } from "react-icons/ti";
+import { IoCheckmark } from "react-icons/io5"; // Make sure to import the correct icon
 
 interface RadialProgressProps {
   percentage: number; // Progress percentage (0-100)
   size?: number; // Size of the progress bar (default: 100px)
   strokeWidth?: number; // Width of the progress bar stroke (default: 10px)
-  color?: string; // Color of the progress bar (default: 'gray')
+  color?: string; // Color of the background progress bar (default: 'gray')
+  outColor?: string; // Color of the progress circle
 }
 
 const RadialProgress: React.FC<RadialProgressProps> = ({
   percentage,
   size = 100,
   strokeWidth = 10,
-  color = "gray",
+  color = 'gray',
+  outColor = 'stroke-indigo-500',
 }) => {
   // Calculate the circumference of the circle
-  const circumference = 2 * Math.PI * (40 - strokeWidth / 2);
+  const radius = 50 - strokeWidth / 2;
+  const circumference = 2 * Math.PI * radius;
   // Calculate the dash offset based on the percentage
   const dashOffset = circumference - (percentage / 100) * circumference;
 
   return (
-    <div className={`relative w-${size} h-${size}`}>
+    <div className="relative flex items-center justify-center w-[1.8rem] h-[1.8rem]">
       <svg className="w-full h-full" viewBox="0 0 100 100">
         {/* Background circle */}
         <circle
@@ -28,23 +31,35 @@ const RadialProgress: React.FC<RadialProgressProps> = ({
           strokeWidth={strokeWidth}
           cx="50"
           cy="50"
-          r="40"
+          r={radius}
           fill="transparent"
         ></circle>
-        {/* Progress circle */}
+        {/* Overlay circle */}
         <circle
-          className="stroke-red-600"
+          className="stroke-red-800"
           strokeWidth={strokeWidth}
           strokeLinecap="round"
           cx="50"
           cy="50"
-          r="40"
+          r={radius}
+          fill="transparent"
+          strokeDasharray={circumference}
+          strokeDashoffset="1"
+        ></circle>
+        {/* Progress circle */}
+        <circle
+          className={`${outColor}`}
+          strokeWidth={strokeWidth}
+          strokeLinecap="round"
+          cx="50"
+          cy="50"
+          r={radius}
           fill="transparent"
           strokeDasharray={circumference}
           strokeDashoffset={dashOffset}
         ></circle>
       </svg>
-      <TiTick className="h-6 w-6  absolute left-2 top-1/2 transform -translate-y-1/2 bg-red-800 rounded-full text-white text-xl" />
+      <IoCheckmark className="absolute w-[1.3rem] h-[1.3rem] text-white bg-red-800 rounded-full p-1" />
     </div>
   );
 };
--- src/components/Resultpage.tsx
+++ src/components/Resultpage.tsx
@@ -1,14 +1,167 @@
 import React from "react";
 import RadialProgress from "./Circleprogress";
-
+import { HiOutlineExclamationTriangle } from "react-icons/hi2";
+import { IoMdClose } from "react-icons/io";
+import { MdOutlineQuestionMark } from "react-icons/md";
+import { MdElectricBolt } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosStarOutline } from "react-icons/io";
 function Resultpage() {
+  const Accordiondata = [
+    {
+      heading: "Human Layer",
+      content:
+        "Pinochle-Al is not your average cybersecurity firm—we defy convention and dare to tread where others fear. Our elite operatives embark on deep infiltrations into the darkest corners of theweb, preemptively identifying potential threats before they strike. We lead the charge on the offensive, striking fear into the hearts of cyber adversaries and neutralizing threats. Our globallystationed cybersecurity insurgents respond swiftly and decisively to emerging threats, ensuring unparalleled protection for your digital assets.",
+      percentage: 50,
+      color: "string",
+      outColor: "stroke-slate-400",
+    },
+    {
+      heading: "Perimeter Security",
+      content:
+        "Pinochle-Al is not your average cybersecurity firm—we defy convention and dare to tread where others fear. Our elite operatives embark on deep infiltrations into the darkest corners of theweb, preemptively identifying potential threats before they strike. We lead the charge on the offensive, striking fear into the hearts of cyber adversaries and neutralizing threats. Our globallystationed cybersecurity insurgents respond swiftly and decisively to emerging threats, ensuring unparalleled protection for your digital assets.",
+      percentage: 50,
+      color: "string",
+      outColor: "stroke-slate-400",
+    },
+    {
+      heading: "Perimeter Security",
+      content:
+        "Pinochle-Al is not your average cybersecurity firm—we defy convention and dare to tread where others fear. Our elite operatives embark on deep infiltrations into the darkest corners of theweb, preemptively identifying potential threats before they strike. We lead the charge on the offensive, striking fear into the hearts of cyber adversaries and neutralizing threats. Our globallystationed cybersecurity insurgents respond swiftly and decisively to emerging threats, ensuring unparalleled protection for your digital assets.",
+      percentage: 50,
+      color: "string",
+      outColor: "stroke-slate-400",
+    },
+    {
+      heading: "Endpoint Security",
+      content:
+        "Pinochle-Al is not your average cybersecurity firm—we defy convention and dare to tread where others fear. Our elite operatives embark on deep infiltrations into the darkest corners of theweb, preemptively identifying potential threats before they strike. We lead the charge on the offensive, striking fear into the hearts of cyber adversaries and neutralizing threats. Our globallystationed cybersecurity insurgents respond swiftly and decisively to emerging threats, ensuring unparalleled protection for your digital assets.",
+      percentage: 50,
+      color: "string",
+      outColor: "stroke-slate-400",
+    },
+    {
+      heading: "Application Security",
+      content:
+        "Pinochle-Al is not your average cybersecurity firm—we defy convention and dare to tread where others fear. Our elite operatives embark on deep infiltrations into the darkest corners of theweb, preemptively identifying potential threats before they strike. We lead the charge on the offensive, striking fear into the hearts of cyber adversaries and neutralizing threats. Our globallystationed cybersecurity insurgents respond swiftly and decisively to emerging threats, ensuring unparalleled protection for your digital assets.",
+      percentage: 50,
+      color: "string",
+      outColor: "stroke-slate-400",
+    },
+  ];
+
   return (
-    <div className="border-[2px] rounded-lg border-[#AC47F7]  h-full flex flex-col  py-8 bg-opacity-10 backdrop-filter backdrop-blur-lg  bg-white">
-      <div className="h-10 w-10 ">
-        <RadialProgress percentage={95} size={100} />
+    <div className="w-full">
+      <div className="flex items-center justify-end gap-3 py-2">
+        <div className="bg-[#3a4066] rounded-3xl text-white px-4 py-1 ">
+          <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-transparent  bg-[#AC47F7] text-white flex items-center justify-center ">
+            <MdOutlineQuestionMark className="mx-auto h-[0.9rem] w-[0.9rem]" />
+          </div>
+        </div>
+        <div className="bg-[#3a4066] rounded-3xl text-white px-4 py-1 ">
+          <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-transparent  bg-[#AC47F7] text-white flex items-center justify-center ">
+            <HiOutlineExclamationTriangle className="mx-auto h-[0.9rem] w-[0.9rem]" />
+          </div>
+        </div>
+        <div className="bg-[#3a4066] rounded-3xl text-white px-4 py-1 ">
+          <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-transparent  bg-[#AC47F7] text-white flex items-center justify-center ">
+            <IoMdClose className="mx-auto h-[0.9rem] w-[0.9rem]" />
+          </div>
+        </div>
+        <div className="bg-[#3a4066] rounded-3xl text-white px-4 py-1 ">
+          <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-transparent  bg-[#AC47F7] text-white flex items-center justify-center ">
+            <MdElectricBolt className="mx-auto h-[0.9rem] w-[0.9rem]" />
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-5">
+        {Accordiondata.map((data) => {
+          return (
+            <Customaccordion
+              color={data.color}
+              content={data.content}
+              heading={data.heading}
+              outColor={data.outColor}
+              percentage={data.percentage}
+            />
+          );
+        })}
       </div>
     </div>
   );
 }
 
 export default Resultpage;
+
+interface accordionprops {
+  heading: string;
+  content: string;
+  percentage: number;
+  color: string;
+  outColor: string;
+}
+
+const Customaccordion: React.FC<accordionprops> = ({
+  heading,
+  content,
+  percentage,
+  color,
+  outColor,
+}) => {
+  return (
+    <div
+      className="flex flex-col  group transition ease-in-out duration-200 cursor-pointer focus-within:bg-[#1a2b67]  bg-white w-full px-3 h-full border-[2px] rounded-lg border-[#AC47F7] border-l-[0.9rem]  py-8 bg-opacity-10 backdrop-filter backdrop-blur-lg duration-300"
+      tabIndex={0}
+    >
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-white text-md tracking-wide  transition ease-in-out duration-100">
+          {heading}
+        </h1>
+        <div className="flex items-center justify-center gap-4">
+          <div>
+            <RadialProgress
+              percentage={percentage}
+              size={10}
+              outColor={outColor}
+            />
+          </div>
+          <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-transparent bg-[#eb886a] text-white flex items-center justify-center">
+            <IoMdClose className="mx-auto h-[0.9rem] w-[0.9rem]" />
+          </div>
+          <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-transparent bg-[#8586be] text-white flex items-center justify-center">
+            <MdElectricBolt className="mx-auto h-[0.9rem] w-[0.9rem]" />
+          </div>
+          <div className="h-[1.7rem] w-[1.7rem] rounded-full bg-transparent border border-[#AC47F7] text-[#AC47F7] flex items-center justify-center focus:bg-red-800 focus:text-white">
+            <IoIosArrowForward className="h-[1rem] w-[1rem] group-focus:rotate-90 transition ease-out duration-150" />
+          </div>
+        </div>
+      </div>
+      <div className="hidden group-focus:block group-focus:h-full   group-focus:h-10 transition ease-in-out duration-700 px-1 py-3 ">
+        <div className="bg-opacity-10 backdrop-filter backdrop-blur-lg bg-white w-full h-1 rounded-full "></div>
+        <div className="flex items-center justify-start gap-5 w-full py-5">
+          <button
+            className={`flex items-center justify-center  border border-[#4D66D0] gap-2 px-4 py-1 rounded-sm text-md   `}
+          >
+            <IoIosStarOutline className="text-white h-6 w-6 " />
+
+            <span className={`text-white lg:block  hidden font-thin`}>
+              Summary
+            </span>
+          </button>
+          <button
+            className={`flex items-center justify-center  border border-[#4D66D0] gap-2 px-4 py-1 rounded-sm text-md `}
+          >
+            <IoIosStarOutline className="text-white h-6 w-6" />
+
+            <span className={`font-thin text-white hidden lg:block `}>
+              CyperKarma GPT
+            </span>
+          </button>
+        </div>
+        <div>
+          <p className="text-white tracking-wide text-md">{content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
